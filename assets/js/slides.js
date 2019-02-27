2814b5cf04c5ed046e465e87321389761917566f
Chee32 <ianfoulk1@gmail.com>
2019-02-27 16:37:27 -0500

    Finished slider js
    Created a slider that can pause when hovered over, auto runs, and has navagation by arrows or by section.
    
    Still need to style slider.

--- assets/js/slides.js
+++ assets/js/slides.js
@@ -1,28 +1,61 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+slider = document.getElementById("slider_1");
+slider.onmouseover = pauseSlider;
+slider.onmouseout = function() { slider.setAttribute("data-slidetimer", setTimeout(runSlides, 5000) ); }// Change image after 5 seconds
+
+runSlides();
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  slider.setAttribute("data-slideindex", parseInt(slider.getAttribute("data-slideindex")) + n);
+  showSlides();
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  slider.setAttribute("data-slideindex", n.toString());
+  showSlides();
 }
 
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
+  
+  if (parseInt(slider.getAttribute("data-slideindex")) > slides.length) {slider.setAttribute("data-slideindex" , "1")} 
+  if (parseInt(slider.getAttribute("data-slideindex")) < 1) {slider.setAttribute("data-slideindex" , slides.length)}
+  
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  
+  slides[parseInt(slider.getAttribute("data-slideindex"))-1].style.display = "block"; 
+  dots[parseInt(slider.getAttribute("data-slideindex"))-1].className += " active";
+
+}
+
+function runSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slider.setAttribute("data-slideindex", parseInt(slider.getAttribute("data-slideindex")) + 1 );
+  if (parseInt(slider.getAttribute("data-slideindex")) > slides.length) {slider.setAttribute("data-slideindex" , "1")}
+   
+  slides[parseInt(slider.getAttribute("data-slideindex"))-1].style.display = "block";
+  dots[parseInt(slider.getAttribute("data-slideindex"))-1].className += " active"; 
+  slider.setAttribute("data-slidetimer", setTimeout(runSlides, 10000) ); // Change image every 10 seconds
+
+}
+
+function pauseSlider() {
+  clearTimeout(parseInt(slider.getAttribute("data-slidetimer")));
 }
